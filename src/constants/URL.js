50ab3f29e68c559267dbed5fadd50bd5a4f9bff9
mnogoruk <lionless072@gmail.com>
2021-07-05 18:19:51 +0300

    server url

--- src/constants/URL.js
+++ src/constants/URL.js
@@ -1,4 +1,4 @@
-const _SERVER_URL = "http://127.0.0.1:8000/"
+const _SERVER_URL = "https://ancient-temple-39835.herokuapp.com/"
 export const GEO_SERVER_URL = _SERVER_URL + "geo/"
 export const ROUTES_SERVER_URL = _SERVER_URL + "route/"
 
