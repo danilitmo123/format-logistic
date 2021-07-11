045073f3cf58b44a47bd24043c4c746d012c1ef7
mnogoruk <lionless072@gmail.com>
2021-07-11 12:00:37 +0300

    removing hubroute, but need to fix

--- src/components/AdminPage/AllHubsPage/index.js
+++ src/components/AdminPage/AllHubsPage/index.js
@@ -1,4 +1,4 @@
-import React,{useEffect,useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import HubsItem from "./HubsItem";
 
@@ -12,68 +12,68 @@ import {ADMIN_SERVER_URL} from "../../../constants/URL";
 
 const AllHubsPage = ({setEditing, allHubsInfo, setAllHubs, setId, hubId}) => {
 
-  const [loading, setLoading] = useState(true)
+    const [loading, setLoading] = useState(true)
 
-  const getHubs = () => {
-    axios.get(`${ADMIN_SERVER_URL}admin-routes/?short`)
-        .then(res => {setAllHubs([res.data])})
-    setLoading(false)
-  }
+    const getHubs = () => {
+        setLoading(true)
+        axios.get(`${ADMIN_SERVER_URL}admin-routes/?short`)
+            .then(res => {
+                setAllHubs([res.data])
+            })
+        setLoading(false)
+    }
 
-  const setEditingHandler = () => {
-    setEditing(false)
-  }
+    const setEditingHandler = () => {
+        setEditing(false)
+    }
 
-  const deleteHubHandler = (i) => {
-    const newData = [...allHubsInfo.slice(0, i), ...allHubsInfo.slice(i + 1)]
-    console.log(newData)
-    // setAllHubs(newData)
-    // axios.delete(`${ADMIN_SERVER_URL}admin-routes/${hubId}/`)
-    //     .then(res => console.log(res))
-  }
+    const deleteHubHandler = (i) => {
+        axios.delete(`${ADMIN_SERVER_URL}admin-routes/${hubId}/`)
+            .then(res => {
+                console.log(res)
+                getHubs()
+            })
+    }
 
-  useEffect(() => {
-    getHubs()
-  }, [])
 
-  useEffect(() => {
-    deleteHubHandler()
-  }, [hubId])
+    useEffect(() => {
+        getHubs()
+    }, [])
 
- return (
-    <section className={'all-hubs-page-wrapper'}>
-      <div className={'hub-title'}>Хабовые плечи</div>
-      {loading ?
-          <div className={'loading-wrapper'}>
-            <img src={loader} alt="loader"/>
-            <div className={'loader-text'}>Загрузка...</div>
-          </div>
-          :
-          <div className={'all-hubs-wrapper'}>
-            {
-              allHubsInfo[0] ?
-                  <div className={'hubs'}>
+    return (
+        <section className={'all-hubs-page-wrapper'}>
+            <div className={'hub-title'}>Хабовые плечи</div>
+            {loading ?
+                <div className={'loading-wrapper'}>
+                    <img src={loader} alt="loader"/>
+                    <div className={'loader-text'}>Загрузка...</div>
+                </div>
+                :
+                <div className={'all-hubs-wrapper'}>
                     {
-                      allHubsInfo[0].map((item, index) => <HubsItem
-                          deleteHub={deleteHubHandler}
-                          index={index}
-                          key={item.id}
-                          setEditing={setEditing}
-                          hub={item}
-                          setId={setId}/>)
+                        allHubsInfo[0] ?
+                            <div className={'hubs'}>
+                                {
+                                    allHubsInfo[0].map((item, index) => <HubsItem
+                                        deleteHub={deleteHubHandler}
+                                        index={index}
+                                        key={item.id}
+                                        setEditing={setEditing}
+                                        hub={item}
+                                        setId={setId}/>)
+                                }
+                            </div>
+                            :
+                            ''
                     }
-                  </div>
-                  :
-                  ''
-            }
 
-          </div>
-      }
-      <Link to={'/admin/create-hub'}>
-        <button className={'create-hubs-button'} onClick={setEditingHandler}>Создать плечо</button>
-      </Link>
-    </section>
- );
+                </div>
+            }
+            <Link to={'/admin/create-hub'}>
+                <button className={'create-hubs-button'} onClick={setEditingHandler}>Создать плечо</button>
+            </Link>
+        </section>
+    );
 };
 
 export default AllHubsPage;
