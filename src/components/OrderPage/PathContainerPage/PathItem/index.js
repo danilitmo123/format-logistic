627249f859961914d9191b82cfaa1141779b7132
danilitmo123 <danilitmo@gmail.com>
2021-08-15 00:53:37 +0300

    modify info-block in PathItem

--- src/components/OrderPage/PathContainerPage/PathItem/index.js
+++ src/components/OrderPage/PathContainerPage/PathItem/index.js
@@ -81,10 +81,13 @@ const PathItem = ({path, setIndex, index, volume, weight}) => {
           <div>
             <div className={'route-info-title'}>Информация о грузе</div>
             <div className={'route-time-and-cost-wrapper'}>
-              <div className={'route-total-cost'}>Цена: {path.total_cost}€/{(path.total_cost * 1.18).toFixed(2)}$</div>
-              <div className={'route-total-duration'}>Время прибытия: {(path.total_duration.min).toFixed(0)} - {(path.total_duration.max).toFixed(0)} дней</div>
               <div className={'route-total-weight'}>Вес: {weight} кг</div>
               <div>Объем: {volume} м³</div>
+            </div>
+            <div className={'route-info-title'}>Информация о маршруте</div>
+            <div className={'route-time-and-cost-wrapper'}>
+              <div className={'route-total-cost'}>Цена: {path.total_cost}€/{(path.total_cost * 1.18).toFixed(2)}$</div>
+              <div className={'route-total-duration'}>Время прибытия: {(path.total_duration.min).toFixed(0)} - {(path.total_duration.max).toFixed(0)} дней</div>
             </div>
           </div>
           <button className={'choose-button'} onClick={() => setIndex(index)}>Выбрать</button>
